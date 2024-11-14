cf71d12d648de6445f7dfc13e5d8512989d91330
ning_guanyi <ning_guanyi@haima.me>
2024-11-14 16:12:47 +0800

    插件封装 plugin-mock 用于单元测试

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 - [x] Axum "Hello World"
 - [x] Extism "Hello World"，插件函数（plugin_fn）。
 - [x] Extism 宿主函数（host_fn）
+- [x] 插件封装 plugin-mock 用于单元测试
 - [ ] 插件配置清单
--- plugins/hello-plugin/src/lib.rs
+++ plugins/hello-plugin/src/lib.rs
@@ -9,14 +9,42 @@ extern "ExtismHost" {
 
 #[plugin_fn]
 pub fn process(input: PluginArgs) -> FnResult<String> {
-    let key = "hello".to_string();
-    let old = unsafe { kv_read(key.clone())? };
+    let date = input.date;
+    let args = input.args;
+    let kv_read = |key| unsafe { kv_read(key) };
+    let kv_write = |key, v| unsafe { kv_write(key, v) };
 
-    let new = old + 1;
-    unsafe {
-        kv_write(key, new)?;
-    }
-    config::get("key")?;
-    let res = format!("input: {:?}\n<br/>\n value:{}, {}", input, old, new);
+    let res = plugin(&date, &args, kv_read, kv_write)?;
     Ok(res)
+}
+pub fn plugin(
+    date: &str,
+    args: &str,
+    kv_read: fn(String) -> Result<i64, extism_pdk::Error>,
+    kv_write: fn(String, i64) -> Result<Option<i64>, extism_pdk::Error>,
+) -> Result<String, extism_pdk::Error> {
+    let key = "hello".to_string();
+    let old: i64 = kv_read(key.clone())?;
+
+    let new: i64 = old + 1;
+    kv_write(key, new)?;
+    println!(
+        "input:date={}, args={:?}\n\tvalue: old={}, new{}",
+        date, args, old, new
+    );
+    Ok(new.to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::plugin;
+
+    #[test]
+    fn test_process() -> Result<(), extism_pdk::Error> {
+        let date = "2024-11-14 14:28:00.000";
+        let args = "";
+        let x = plugin(date, args, plugin_mock::kv_read, plugin_mock::kv_write)?;
+        assert_eq!("1", &x);
+        Ok(())
+    }
 }
